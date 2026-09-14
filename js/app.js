502509d08a60d8ff6df2bc9218253cb4387defc8
saloncopoya <151898433+saloncopoya@users.noreply.github.com>
2026-09-13 20:38:00 -0600

    Increase distance threshold for route transfers

--- js/app.js
+++ js/app.js
@@ -1718,7 +1718,7 @@ const url = location.origin + '/share/m/' + id;
           endRoutes.forEach(r3 => {
             if (r3.id === r2.id || r3.id === r1.id) return;
             const i23 = routesMinDistance(r2, r3);
-            if (i23.dist > 300) return;
+            if (i23.dist > 400) return;
             chains.push({
               type: 'transfer',
               legs: [r1, r2, r3],
